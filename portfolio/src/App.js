a3c2daf7b843f8ea93d130cac14e971ee65653ac
milesuno <milesoluku@gmail.com>
2019-08-09 09:11:24 +0100

    bug fix

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import LandingPage from './components/landing-page/landing-page';
 import NavBar from './components/navbar/nav-bar';
 import UXDesigns from './components/ux-designs/ux-designs';
-import Projects from './components/projects/projects';
+import Projects from './components/projects/Projects';
 import AboutMe from './components/about-me/about-me';
 
 function App() {
