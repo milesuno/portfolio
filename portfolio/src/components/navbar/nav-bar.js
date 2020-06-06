21557f50ff7b66cae24f312def8e73ef28bb65e4
milesuno <milesoluku@gmail.com>
2020-06-06 20:05:43 +0100

    NavBar menu Link render debugged - Large and Short menu

--- portfolio/src/components/navbar/nav-bar.js
+++ portfolio/src/components/navbar/nav-bar.js
@@ -7,7 +7,6 @@ import uxData from "../../data/ux-data.json";
 import socialMediaData from "../../data/social-media-data.json";
 import menu from "../../landing-video/icons8-menu-squared-96.png";
 import closeMenu from "../../landing-video/icons8-close-window-96.png";
-import NavbarMenu from "../navbar-menu/navbar-menu";
 
 class NavBar extends Component {
 	constructor(props) {
@@ -29,23 +28,22 @@ class NavBar extends Component {
 	//FIX: display menu above 736px -> display: flex; flex-direction: row;
 	componentDidMount() {
 		const navBtns = document.querySelectorAll("li.nav-item");
-		// const correctLeave = document.querySelectorAll("li.nav-item");
 
 		navBtns.forEach((navBtn) => {
-			console.dir(navBtn)
-			navBtn.addEventListener("mouseenter", this.handleMouseEnter, true);
-			navBtn.addEventListener("mouseleave", this.handleMouseLeave, true);
+			console.dir(navBtn);
+			navBtn.addEventListener("mouseenter", this.handleMouseEnter);
+			navBtn.addEventListener("mouseleave", this.handleMouseLeave);
 		});
 	}
 
 	handleMouseEnter = (e, clickEvent = false) => {
 		console.log("Enter", e.target);
-		if (!e.target.name) return;
+		if (!e.target.children[0].name) return;
 		const nav = document.querySelector(".nav");
 		const navBtns = document.querySelectorAll("li.nav-item");
 		const allDropdownList = document.querySelectorAll(`.dropdown-list`);
 		const dropdownList = document.querySelector(
-			`.dropdown-list.${e.target.name}`
+			`.dropdown-list.${e.target.children[0].name}`
 		);
 		const dropdownBackground = document.querySelector(
 			".dropdownBackground"
@@ -103,12 +101,12 @@ class NavBar extends Component {
 	};
 
 	handleMouseLeave = (e, clickEvent = false) => {
-		if (!e.target.name) return;
+		if (!e.target.children[0].name) return;
 		console.log("Leave", e.target);
 		const navBtns = document.querySelectorAll("li.nav-item");
 
 		const dropdownList = document.querySelector(
-			`.dropdown-list.${e.target.name}`
+			`.dropdown-list.${e.target.children[0].name}`
 		);
 		const dropdownBackground = document.querySelector(
 			".dropdownBackground"
@@ -127,8 +125,31 @@ class NavBar extends Component {
 				this.handleMouseEnter(e, true)
 			);
 		}
-		console.log({dropdownList})
+		console.log({ dropdownList });
 		// dropdownList.style.flexDirection = "column";
+	};
+
+	handleMouseClick = (e) => {
+		//use target to set display of li.items
+		e.preventDefault();
+		console.log("Click Event", e.target);
+
+		const dropdownList = document.querySelector(
+			`.dropdown-list.${e.target.name}`
+		);
+
+		console.log({ dropdownList });
+
+		if (!this.state.menuToggle) {
+			console.log("Display: flex");
+			dropdownList.style.setProperty("display", "flex");
+			dropdownList.style.setProperty("flex-direction", "column");
+		} else {
+			console.log("Display: none");
+			dropdownList.style.setProperty("display", "none");
+		}
+
+		this.setState({ menuToggle: !this.state.menuToggle });
 	};
 
 	showMenuButtons = () => {
@@ -140,20 +161,31 @@ class NavBar extends Component {
 		const shortMenuCoords = document
 			.querySelector(".short-nav")
 			.getBoundingClientRect();
+		const collaspedMenuBtn = document.querySelector(".collasped-menu-btn");
+		const linkBtns = document.querySelectorAll(".link");
 		// const menuBtnLinks = document.querySelectorAll("a.link.lg-display");
 		// menuBtnLinks.forEach((menuBtn) => (menuBtn.href = ""));
 
 		// console.log({ shortMenuCoords, menuBtnLinks });
 
 		console.log({
-			// collaspedMenuBtn,
+			collaspedMenuBtn,
 			navBtns,
 			navContent,
-			// navBar,
+			linkBtns,
 			dropdownLists,
 		});
 
+		linkBtns.forEach((btn) => (btn.href = ""));
 		if (this.state.menuToggle) {
+			collaspedMenuBtn.style.setProperty("display", "flex");
+			navContent.style.setProperty("display", "flex");
+			navContent.style.setProperty("flex-direction", "column");
+			navContent.style.setProperty("position", "absolute");
+			navContent.style.setProperty("transform", "translate(0, 0%)");
+			navContent.style.setProperty("width", "100%");
+			navContent.style.setProperty("top", "60px");
+			navContent.style.setProperty("opacity", 1);
 			// navContent.style.setProperty("top", `${shortMenuCoords.bottom}px`);
 
 			dropdownLists.forEach((dropdownList) => {
@@ -164,58 +196,33 @@ class NavBar extends Component {
 
 			navBtns.forEach((navBtn) => {
 				console.log("NAVBTN");
-				console.dir(navBtn);
+				let button = navBtn.firstChild;
+				button.style.setProperty("width", "100%");
+				button.style.setProperty("padding", "10px");
+				navBtn.removeEventListener("mouseenter", this.handleMouseEnter);
+				navBtn.removeEventListener("mouseleave", this.handleMouseLeave);
 
-				// let button = navBtn.firstChild;
-				// button.style.setProperty("width", "100%");
-				// button.style.setProperty("padding", "10px");
-				navBtn.removeEventListener(
-					"mouseenter",
-					this.handleMouseEnter,
-					true
-				);
-				navBtn.removeEventListener(
-					"mouseleave",
-					this.handleMouseLeave,
-					true
-				);
-
-				navBtn.addEventListener("click", (e) =>
-					this.handleMouseEnter(e, true)
-				);
+				navBtn.addEventListener("click", this.handleMouseClick);
+				console.log({ button });
 			});
 			this.setState({ menuToggle: false });
 		} else {
+			collaspedMenuBtn.style.setProperty("display", "flex");
+			navContent.style.setProperty("display", "none");
+
+			// navContent.style.setProperty("flex-direction", "row");
+			// navContent.style.setProperty("position", "relative");
+
+			// navContent.style.setProperty("transform", "translate(0, 0%)");
+
 			dropdownLists.forEach((dropdownList) => {
 				dropdownList.style.setProperty("background", "");
 				dropdownList.style.setProperty("width", "125px");
 				dropdownList.style.setProperty("margin", "10px");
 			});
 
-			navBtns.forEach((navBtn) => {
-				console.log("Re add mouseenter", navBtn)
-				navBtn.removeEventListener("click", (e) =>
-					this.handleMouseEnter(e)
-				);
-				navBtn.removeEventListener("click", (e) =>
-					this.handleMouseEnter(e)
-				);
-
-				navBtn.addEventListener(
-					"mouseenter",
-					this.handleMouseEnter,
-					true
-				);
-				navBtn.addEventListener(
-					"mouseleave",
-					this.handleMouseEnter,
-					true
-				);
-			});
 			this.setState({ menuToggle: true });
 		}
-
-		// if (this.state.menuToggle) navContent.style.setProperty("opacity", "1");
 	};
 
 	render() {
@@ -245,13 +252,110 @@ class NavBar extends Component {
 							<img src={closeMenu} />
 						)}
 					</div>
-					<NavbarMenu
-						projectData={this.state.project}
-						uxData={this.state.ux}
-						courseData={this.state.course}
-						socialMediaData={this.state.socialMedia}
-						menuToggle={this.state.menuToggle}
-					/>
+					<ul className="nav-content">
+						<Link className="link lg-display" to="/projects">
+							<li className="nav-item project">
+								<a className="button" name="project">
+									PROJECTS
+								</a>
+								<ul className="dropdown-list project">
+									<li className="short-nav-all-items">
+										<Link to={`/projects`}>
+											All Projects
+										</Link>
+									</li>
+									{
+										/*Loop through all projects and render a list of these items as <li>*/
+										projectData.map((project) => {
+											// console.log(project);
+											return (
+												<li className="dropdown-item">
+													<Link
+														to={`/projects/${project.id}`}
+													>
+														{project.name}
+													</Link>
+												</li>
+											);
+										})
+									}
+								</ul>
+							</li>
+						</Link>
+						<Link className="link lg-display" to="/uxdesigns">
+							<li className="nav-item ux">
+								<a className="button" name="ux">
+									UX/UI DESIGNS
+								</a>
+								<ul className="dropdown-list ux">
+									<li className="short-nav-all-items">
+										<Link to={`/uxdesigns`}>
+											All UX designs
+										</Link>
+									</li>
+									{
+										/*Loop through all projects and render a list of these items as <li>*/
+										uxData.map((ux) => {
+											// console.log(ux);
+											return (
+												<li className="dropdown-item">
+													<Link
+														to={`/uxdesigns/${ux.id}`}
+													>
+														{ux.name}
+													</Link>
+												</li>
+											);
+										})
+									}
+								</ul>
+							</li>
+						</Link>
+						<Link className="link lg-display" to="/courses">
+							<li className="nav-item courses">
+								<a className="button" name="courses">
+									COURSES
+								</a>
+								<ul className="dropdown-list courses">
+									<li className="dropdown-item">
+										<Link to={`/courses`}>All Courses</Link>
+									</li>
+									{
+										/*Loop through all projects and render a list of these items as <li>*/
+										courseData.map((course) => {
+											// console.log(course);
+											return (
+												<li className="dropdown-item">
+													<Link
+														to={`/courses/${course.id}`}
+													>
+														{course.name}
+													</Link>
+												</li>
+											);
+										})
+									}
+								</ul>
+							</li>
+						</Link>
+						<Link className="link lg-display" to="/aboutme">
+							<li className="nav-item about-me">
+								<a className="button" name="about-me">
+									ABOUT ME
+								</a>
+								<ul className="dropdown-list about-me">
+									{socialMediaData.map((media) => {
+										// console.log(course);
+										return (
+											<li className="dropdown-item">
+												<a>{media.medium}</a>
+											</li>
+										);
+									})}
+								</ul>
+							</li>
+						</Link>
+					</ul>
 				</div>
 			</nav>
 		);
