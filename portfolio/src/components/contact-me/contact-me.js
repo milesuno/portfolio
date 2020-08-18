b1021f1d3dd38b810992d04634d2b3edc2f45e66
milesuno <milesoluku@gmail.com>
2020-08-18 11:41:15 +0100

    SetUp: DevProcess + ContactMe v2

--- portfolio/src/components/contact-me/contact-me.js
+++ portfolio/src/components/contact-me/contact-me.js
@@ -1,40 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Helmet from "react-helmet";
-import NavBar from "../navbar/nav-bar"
-import FooterNav from '../footer-nav/footer-nav';
+import NavBar from "../navbar/nav-bar";
+import FooterNav from "../footer-nav/footer-nav";
+import SocialMediaNav from "../social-media-nav/social-media-nav";
+import CV from "../../data/Gideon Miles Oluku - cv6.pdf";
+
 
 class ContactMe extends Component {
-    state = {};
-    render() {
-        return (
-            <>
-                <Helmet>
-                    <title>Portfolio - Contact</title>
-                    <meta
-                        name="description"
-                        content="About me page, highlighting resume achievements and channels to contact me"
-                    />
-                </Helmet>
-                <div className="page-wrapper">
-                    <div className="page-width">
-                        <header>
-                            <NavBar />
-                        </header>
-                        <main className="main-wrapper">
-                            <h1>Contact</h1>
-                            <h2>Additional Socials:</h2>
-                            <h2>Email:</h2>
-                            <h2>Enquiries:</h2>
-                            <h2>Download CV</h2>
-                        </main>
-                        <footer>
-                            <FooterNav />
-                        </footer>
-                    </div>
-                </div>
-            </>
-        );
-    }
+	state = {};
+	render() {
+		return (
+			<>
+				<Helmet>
+					<title>Portfolio - Contact</title>
+					<meta
+						name="description"
+						content="About me page, highlighting resume achievements and channels to contact me"
+					/>
+				</Helmet>
+				<div className="page-wrapper">
+					<div className="page-width">
+						<header>
+							<NavBar />
+						</header>
+						<main className="main-wrapper">
+							<h1>Contact</h1>
+							<h2>Additional Contact:</h2>
+							<SocialMediaNav />
+							<h2>Email:</h2>
+							<p>Milesoluku@gmail.com</p>
+							<h2>Enquiries:</h2>
+							<form className="enquiries-form">
+								<label>
+									Name:
+									<input type="text" />
+								</label>
+								<label>
+									Subject:
+									<select>
+										<option>Option 1</option>
+										<option>Option 2</option>
+										<option>Option 3</option>
+									</select>
+								</label>
+								<label>
+									Message:
+									<input type="text" />
+								</label>
+							</form>
+							<a className="download" href={CV} target="#">
+								<button>View CV</button>
+							</a>{" "}
+						</main>
+						<footer>
+							<FooterNav />
+						</footer>
+					</div>
+				</div>
+			</>
+		);
+	}
 }
 
 export default ContactMe;
--- portfolio/src/components/dev-process/DevProcess.js
+++ portfolio/src/components/dev-process/DevProcess.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Helmet from "react-helmet";
 import NavBar from "../navbar/nav-bar";
-import FooterNav from '../footer-nav/footer-nav';
+import FooterNav from "../footer-nav/footer-nav";
 
 class DevProcess extends Component {
 	state = {};
@@ -21,19 +21,44 @@ class DevProcess extends Component {
 							<NavBar />
 						</header>
 						<main className="main-wrapper">
-                            <h1>Development Process</h1>
-                            <h2>Discovery</h2>
-                            <h2>Brain Storming</h2>
-                            <h2>Plan Review</h2>
-                            <h2>Prototyping</h2>
-                            <h2>Prototype Review</h2>
-                            <h2>Development</h2>
-                            <h2>Acceptance Review</h2>
-                            <h2>Deployment</h2>
-                        </main>
-                        <footer>
-                            <FooterNav />
-                        </footer>
+							<h1>Development Process</h1>
+							<section className="discovery">
+								<h2>Discovery</h2>
+								<p>Project Objectives</p>
+								<p>Project Branding</p>
+								<p>Existing Examples</p>
+							</section>
+							<section className="planning">
+								<h2>Brain Storming</h2>
+								<p>Mood Board</p>
+								<p>Color Scheme</p>
+								<p>Project Scoping</p>
+							</section>
+							<section className="review">
+								<h2>Plan Review</h2>
+							</section>
+							<section className="prototyping">
+								<h2>Prototyping</h2>
+								<p>Wireframing</p>
+								<p>MVP</p>
+								<p>Project Timeline</p>
+							</section>
+							<section className="review">
+								<h2>Prototype Review</h2>
+							</section>
+							<section className="development">
+								<h2>Development</h2>
+                                <p>Sprints</p>
+                                <p>Unit testing</p>
+							</section>
+							<section className="review">
+								<h2>Acceptance Review</h2>
+							</section>
+							<h2>Deployment</h2>
+						</main>
+						<footer>
+							<FooterNav />
+						</footer>
 					</div>
 				</div>
 			</>
